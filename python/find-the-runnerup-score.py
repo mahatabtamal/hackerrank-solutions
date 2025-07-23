179eb7af8e644b64ebd1b36a7338d25ab1d4de3a
tamal <mahatab.tamal@proton.me>
2025-07-23 17:36:02 +0600

    fixed the runner-up solution in list index. -2 instead of 1

--- python/find-the-runnerup-score.py
+++ python/find-the-runnerup-score.py
@@ -3,6 +3,6 @@ if __name__ == '__main__':
     array = map(int, input().split())
     new_set = list(set(num for num in array))
     new_set.sort()
-    print(new_set[1])
+    print(new_set[-2])
     
     
