38a4170db2cf787a2c5356983cb2369a456179c8
tamal <mahatab.tamal@proton.me>
2025-07-24 12:06:13 +0600

    Using nested lists as required

--- python/nested_list.py
+++ python/nested_list.py
@@ -1,17 +1,30 @@
 if __name__ == '__main__':
-
+    # The first part is obvious
     n = int(input())
-
-    students = dict()
+    students = []  
 
     for _ in range(n):
         name = input()
         score = float(input())
+        students.append([name, score]) # Append the name and score as a sublist
 
-        students.update({name: score})
-        sorted_students = sorted(set(students.values))
+    # Get all unique scores and sort them to find the second lowest
+    # We use a set comprehension to get unique scores, then sort them
+    sorted_unique = sorted({score for name, score in students})
 
-        for i in sorted(students):
-            if students[i] == sorted_students[1]:
-                print(i)
-        
+    # The second lowest score will be at index 1
+    if len(sorted_unique) < 2:
+        print("No second lowest.")
+    else:
+        second_lowest = sorted_unique[1]
+
+        # Collect all names with the second lowest score
+        # then sort and print them
+        second_lowest_people = []
+        for name, score in students:
+            if score == second_lowest:
+                second_lowest_people.append(name)
+
+        # Print the names in alphabetical order
+        for name in sorted(second_lowest_people):
+            print(name)
